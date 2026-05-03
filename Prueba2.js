05ca1b44ed8077889122d023c7d41369611107fa
itachi90geor <castroperezgeorge@gmail.com>
2026-05-02 20:14:36 -0500

    Turismo Ica

--- Prueba2.js
+++ Prueba2.js
@@ -1,13 +1,8 @@
 /* Formulario de Fechas fijo y libre */
 function cambiarSelectorFecha() {
-    // Se indica el tipo de tour seleccionado por el usuario (grupal o privado)
     var tipoTour = document.getElementById("tipo-tour").value;
-    
-    // Obtenemos los dos contenedores de fechas que creamos en el HTML
     var cajaFechasFijas = document.getElementById("contenedor-fechas-fijas");
     var cajaFechaLibre = document.getElementById("contenedor-fecha-libre");
-
-    // Condicional para mostrar u ocultar los contenedores según el tipo de tour seleccionado
     if (tipoTour === "grupal") {
         cajaFechasFijas.style.display = "block";
         cajaFechaLibre.style.display = "none";
@@ -17,16 +12,20 @@ function cambiarSelectorFecha() {
     }
 }
 
-// Esperar a que el DOM esté completamente cargado
-// Esperamos a que la página cargue totalmente
+/* Menú hamburguesa */
 document.addEventListener('DOMContentLoaded', () => {
     const boton = document.getElementById('botonMenu');
     const menu = document.getElementById('menuPrincipal');
-
+    const body = document.body;
     if (boton && menu) {
         boton.addEventListener('click', () => {
             menu.classList.toggle('activo');
             boton.classList.toggle('abierto');
+            if (menu.classList.contains('activo')) {
+                body.classList.add('no-scroll');
+            } else {
+                body.classList.remove('no-scroll');
+            }
         });
     }
 });
